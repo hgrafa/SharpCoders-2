e0aca1b63d0ddbbafd5dd69dc392510d3a3a7b0f
hgrafa <hugorafell@gmail.com>
2024-01-12 20:50:38 -0300

    solucao linear resto 2 beecrowd

---
+++ Beecrowd/Resto2_1075/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program {
+  public static void Main() {
+
+    int n = int.Parse(Console.ReadLine());
+
+    // a % b = resto de a divido por b
+    for(int val = 1; val <= 10000; val++) {
+      if(val%n == 2) {
+        Console.WriteLine(val);
+      }
+    }
+
+    13*? + 2
+  }
+}
