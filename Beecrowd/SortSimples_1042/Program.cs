6d9a8fb41d0aa6e99af35a6db6c3f49555b53fa4
hgrafa <hugorafell@gmail.com>
2024-01-12 19:59:47 -0300

    segunda solucao sort simples com min e max

--- Beecrowd/SortSimples_1042/Program.cs
+++ Beecrowd/SortSimples_1042/Program.cs
@@ -9,40 +9,20 @@ public class Program {
     int b =int.Parse(entradas[1]);
     int c =int.Parse(entradas[2]);
 
-    int menor, meio, maior;
+    int menor = Math.Min(Math.Min(a, b), c);
+    int maior = Math.Max(Math.Max(a, b), c);
+    int meio;
 
-    if(a < b && a < c){
-      Console.WriteLine(a);
+    if(a != menor && a != maior)
+      meio = a;
+    else if(b != menor && b != maior)
+      meio = b
+    else 
+      meio = c;
 
-      if(b < c) {// a < b < c
-        Console.WriteLine(b);
-        Console.WriteLine(c);
-      } else { // a < c < b
-        Console.WriteLine(c);
-        Console.WriteLine(b);
-      }
-    } else if (b < a && b < c){ 
-      Console.WriteLine(b);
-
-      if(a < c) { // b < a < c
-        Console.WriteLine(a);
-        Console.WriteLine(c);
-      } else {
-        Console.WriteLine(c);
-        Console.WriteLine(a);
-      }
-    } else {
-      Console.WriteLine(c);
-
-      if(a < b) { // c < a < b
-        Console.WriteLine(a);
-        Console.WriteLine(b);
-      } else { // c < b < a
-        Console.WriteLine(b);
-        Console.WriteLine(a);
-      }
-    }
-
+    Console.WriteLine(menor);
+    Console.WriteLine(meio);
+    Console.WriteLine(maior);
     Console.WriteLine();
     Console.WriteLine(a);
     Console.WriteLine(b);
