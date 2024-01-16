7be257a12441bda0dbf6aad863d003c7a1f7d711
hgrafa <hugorafell@gmail.com>
2024-01-15 21:12:56 -0300

    adicionando exemplos de funcoes

---
+++ samples/CRUD_users/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace CRUD_users;
+
+public class Program {
+
+  static void ShowMenu() {
+    Console.WriteLine("Escolha uma opcao abaixo");
+    // 1 - para criar um usuario
+    // 2 - buscar um usuario
+    // 3 - listar os usuarios (sem senha se houver)
+    // 4 - atualizar um usuario
+    // 5 - deletar um usuario
+    // 0 - para sair
+    Console.Write("Digite a opcao desejada: ");
+  }
+
+  static void CriarUsuario() {
+    Console.WriteLine("Usuario Criado");
+  }
+
+  public static void Main () {
+
+    int size = 1000;
+    string[] usernames = new string[size];
+    string[] emails = new string[size];
+    string[] passwords = new string[size];
+
+    ShowMenu();
+    int option = int.Parse(Console.ReadLine());
+
+    while(option != 0) {
+
+      if(option == 1) {
+        CriarUsuario();
+      }
+
+      ShowMenu();
+      option = int.Parse(Console.ReadLine());
+    }
+  }
+
+}
---
+++ samples/Functions/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Functions;
+
+public class Program {
+  //f(x) = x + 2
+  static double SomarDois(double x) {
+    return x + 2;
+  }
+
+  // void = vazio
+  static void SayHello() {
+    Console.WriteLine("Hello!");
+    return;
+  }
+
+  static void SayHelloToSomeone(string name) {
+    Console.WriteLine($"Hello, {name}");
+  }
+
+  // boolean = prefixo Is e Has
+  // even = Par
+  static bool IsEven(int num) {
+    return num%2 == 0;
+  }
+
+  static bool HasInvalidCharacters(string username) {
+    // logica para validar se o username esta com caracteres invalidos
+    return false;
+  }
+
+  static double Somar(double a, double b) {
+    return a + b;
+  }
+
+  public static void Main() {
+    
+    string username = Console.ReadLine();
+
+    // legibilidade
+    if(HasInvalidCharacters(username)) {
+      // faz algo com o username
+      Console.WriteLine("Username Invalido!");
+    } else {
+      Console.WriteLine("Username Valido");
+    }
+
+    Console.WriteLine(SomarDois(5));
+  }
+}
