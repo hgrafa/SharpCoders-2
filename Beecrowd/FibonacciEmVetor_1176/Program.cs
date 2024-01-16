4921261dd1331671d03975c4db85398e8dbe9dca
hgrafa <hugorafell@gmail.com>
2024-01-15 21:12:35 -0300

    adicionando solucoes beecrowd

---
+++ Beecrowd/FibonacciEmVetor_1176/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FibonacciEmVetor_1176;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+
+    ulong[] fib = new ulong[70];
+    int nTestes = int.Parse(Console.ReadLine());
+
+    // valores iniciais
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int _ = 0; _ < nTestes; _++)
+    {
+
+      int n = int.Parse(Console.ReadLine());
+
+      // TODO: melhorar percorrendo apenas os valores ainda nao calculados
+      for(int i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+      }
+
+      Console.WriteLine($"Fib({n}) = {fib[n]}");
+    }
+
+  }
+}
---
+++ Beecrowd/MultiplosDe13_1132/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MultiplosDe13_1132;
+
+public class Program {
+
+  static bool IsMultiplo13(int number) {
+    return number%13 == 0;
+  }
+
+  static int SomaIntervalo(int inicio, int fim) {
+    // calcula pela soma da PA
+    return 0;
+  }
+
+  public static void Main(string[] args)
+  {
+    // quero = tudo - {nao quero}
+    // quero = a soma dos que nao sao mult 13
+    // tudo = somar tudo
+    // nao quero = soma dos multiplos 13
+
+    // 10 15 = 10 + 11 + 12 + 13 + 14 + 15 - 13
+
+    int inicio = int.Parse(Console.ReadLine());
+    int fim = int.Parse(Console.ReadLine());
+
+    if(inicio > fim) {
+      int aux = inicio;
+      inicio = fim;
+      fim = aux;
+    }
+
+    int soma = 0;
+
+    for(int i = inicio; i <= fim; i++) {
+      if(!IsMultiplo13(i))
+        soma += i;
+    }
+
+    Console.WriteLine(soma);
+  }
+
+}
