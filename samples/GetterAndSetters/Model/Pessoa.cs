fdbdbdeb52aa7181b36c4f918c266ec1253f159f
hgrafa <hugorafell@gmail.com>
2024-01-26 20:59:37 -0300

    melhorando com properties

--- samples/GetterAndSetters/Model/Pessoa.cs
+++ samples/GetterAndSetters/Model/Pessoa.cs
@@ -4,28 +4,56 @@ public class Pessoa
 {
   private int _idade;
   private double _altura;
+  private double _peso;
   public string nome;
 
-  public Pessoa(string nome, double altura, int idade)
+  public Pessoa(string nome, double altura, double peso, int idade)
   {
       this.nome = nome;
       _altura = altura;
       _idade = idade;
+      _peso = peso;
   }
 
-  public int GetIdade() {
-    return _idade;
-  }
-
-  public double GetAltura() {
-    return _altura;
-  }
-
-  public void SetAltura(double altura) {
-    if(altura < 0) {
-      throw new Exception("A altura deve ser positiva");
+  public double Altura {
+    get {
+      return _altura;
     }
+    set {
+      if(value < 0) {
+        throw new Exception("A altura deve ser positiva");
+      }
 
-    _altura = altura;
+      _altura = value;
+    }
   }
+
+  public int Idade {
+    get {
+      return _idade;
+    }
+  }
+
+  public double IMC {
+    get {
+      return _peso / (_altura * _altura);
+    }
+  }
+
+  // public void SetAltura(double altura) {
+  //   if(altura < 0) {
+  //     throw new Exception("A altura deve ser positiva");
+  //   }
+
+  //   _altura = altura;
+  // }
+
+  // public int GetIdade() {
+  //   return _idade;
+  // }
+
+  // public double GetAltura() {
+  //   return _altura;
+  // }
+
 }
--- samples/GetterAndSetters/Program.cs
+++ samples/GetterAndSetters/Program.cs
@@ -1,11 +1,13 @@
 ﻿using GetterAndSetters.Model;
 
-Pessoa alice = new Pessoa("Alice da Silva", 1.7, 23);
+Pessoa alice = new Pessoa("Alice da Silva", 1.7, 60, 23);
 
 // nao funciona :)
 // alice.idade = -1000;
-alice.SetAltura(-1.69);
+alice.Altura = 1.69;
+// alice.Idade = 10;
 
 Console.WriteLine(alice.nome);
-Console.WriteLine(alice.GetAltura());
-Console.WriteLine(alice.GetIdade());
+Console.WriteLine(alice.Altura);
+Console.WriteLine(alice.Idade);
+Console.WriteLine($"{alice.IMC:F2}");
