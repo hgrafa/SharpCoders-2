24c1fd261536256f1807e648aaa4ac7fd6d10a39
hgrafa <hugorafell@gmail.com>
2024-01-26 21:35:27 -0300

    resultado final com autoproperties

--- samples/GetterAndSetters/Model/Pessoa.cs
+++ samples/GetterAndSetters/Model/Pessoa.cs
@@ -2,58 +2,23 @@ namespace GetterAndSetters.Model;
 
 public class Pessoa
 {
-  private int _idade;
-  private double _altura;
-  private double _peso;
-  public string nome;
+  public string Nome { get; set; }
+  public int Idade { get; private set; }
+  public double Altura { get; set; }
+  public double Peso { get; set; }
 
-  public Pessoa(string nome, double altura, double peso, int idade)
+  public Pessoa(string nome, int idade, double altura, double peso)
   {
-      this.nome = nome;
-      _altura = altura;
-      _idade = idade;
-      _peso = peso;
-  }
-
-  public double Altura {
-    get {
-      return _altura;
-    }
-    set {
-      if(value < 0) {
-        throw new Exception("A altura deve ser positiva");
-      }
-
-      _altura = value;
-    }
-  }
-
-  public int Idade {
-    get {
-      return _idade;
-    }
+      Nome = nome;
+      Idade = idade;
+      Altura = altura;
+      Peso = peso;
   }
 
   public double IMC {
     get {
-      return _peso / (_altura * _altura);
+      return Peso / (Altura * Altura);
     }
   }
-
-  // public void SetAltura(double altura) {
-  //   if(altura < 0) {
-  //     throw new Exception("A altura deve ser positiva");
-  //   }
-
-  //   _altura = altura;
-  // }
-
-  // public int GetIdade() {
-  //   return _idade;
-  // }
-
-  // public double GetAltura() {
-  //   return _altura;
-  // }
 
 }
