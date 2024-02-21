e90395eb076c3200907830f40e3ad591042a36a6
hgrafa <hugorafell@gmail.com>
2024-02-21 20:11:32 -0300

    removendo Parse e adicionando metodo 'construtor'

--- samples/Arquivos/Model/Student.cs
+++ samples/Arquivos/Model/Student.cs
@@ -7,6 +7,20 @@ public class Student
    public double Score { get; set; }
    public string University { get; set; }
 
+   public Student() {
+   }
+
+   public static Student FromCsv(string csvLine) {
+      string[] studentData = csvLine.Split(',');
+
+      return new Student() {
+        Id = int.Parse(studentData[0]),
+        FirstName = studentData[1],
+        Score = double.Parse(studentData[2]),
+        University = studentData[3]
+      };
+   }
+
     public override string ToString()
     {
         return $"Id: {Id}, FirstName: {FirstName}, Score:{Score}, University: {University}";
