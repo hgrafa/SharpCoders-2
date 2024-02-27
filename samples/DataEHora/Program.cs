6c8a4bff966f7d198bcab5a7f1f080047ef2a233
hgrafa <hugorafell@gmail.com>
2024-02-26 21:08:41 -0300

    adicionando exemplo com datas e horas

---
+++ samples/DataEHora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+DateOnly date = new DateOnly(2024, 2, 26);
+
+DateTime momento = DateTime.Now;
+
+Console.WriteLine(date);
+Console.WriteLine(momento.ToString("dd/MM/yyyy HH:mm"));
+
+Console.WriteLine(momento.ToUniversalTime());
+
+try {
+  DateTime entrada = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+  Console.WriteLine(entrada);
+
+  TimeSpan duracao = entrada.Subtract(DateTime.Now);
+
+  Console.WriteLine(duracao.Hours);
+  Console.WriteLine(duracao.TotalHours);
+
+} catch(FormatException formatException) {
+  Console.WriteLine(formatException.Message);
+}
