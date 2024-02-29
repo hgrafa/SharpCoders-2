4c727e78cf725347d9fb2699a85887dee7965a5e
hgrafa <hugorafell@gmail.com>
2024-02-28 23:11:38 -0300

    adicionando exemplo API

---
+++ samples/backend/IntrumentosMusicais/Model/Person.cs
@@ -0,0 +1,10 @@
+namespace IntrumentosMusicais.Model;
+
+public class Person
+{
+   
+   public string Name { get; set; }
+   public int Age { get; set; }
+   public double Height { get; set; }
+
+}
